632367f66187f7cb28dff635c6c9559ea65c37e4
Solbin Lee <lees15@rose-hulman.edu>
2025-03-23 18:11:41 -0400

    gestures

--- simple_test.py
+++ simple_test.py
@@ -1,13 +1,48 @@
 from time import sleep
 from utils import *
+import keyboard
 
 init_robot()
 
-run_seq("reset")
-sleep(1)
-run_seq("yes")
-sleep(5)
-run_seq("happy")
-sleep(5)
-run_seq("sad")
-sleep(10)
+# run_seq("reset")
+# sleep(1)
+# run_seq("yes")
+# sleep(5)
+# run_seq("happy")
+# sleep(5)
+# run_seq("sad")
+# sleep(10)
+# run_seq("happyyyy")
+# sleep(10)
+
+
+while True:
+    try:
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN:  
+            if event.name == 'left':
+                print("left")
+                run_seq("left")
+                sleep(1)
+            elif event.name == 'right':
+                print("right")
+                run_seq("right")
+                sleep(1)
+            elif event.name == 'down':
+                print("down")
+                run_seq("down")
+                sleep(1)
+            elif event.name == 'up':
+                print("up")
+                run_seq("up")
+                sleep(1)
+            elif event.name == 'r':
+                print("reset")
+                run_seq("reset")
+                sleep(1)
+            elif event.name == 'space':
+                print("space")
+                run_seq("hungry")
+                sleep(1)
+    except KeyboardInterrupt:
+        break
